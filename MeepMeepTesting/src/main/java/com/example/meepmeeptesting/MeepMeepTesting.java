9f229d6b7fe70de953d19694b691ba62115d3d82
Devildogs11206 <devildogs11206@gmail.com>
2023-01-02 18:34:52 -0600

    added odometry pathfinding to meepmeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,32 +1,29 @@
 package com.example.meepmeeptesting;
 
-import static com.example.meepmeeptesting.Game.Plan.A;
-import static com.example.meepmeeptesting.Game.Plan.B;
-import static com.example.meepmeeptesting.Game.Side.LEFT;
-
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequenceBuilder;
 
-public class MeepMeepTesting {
-    private static final Game.Side side = LEFT;
-    private static final Game.Plan plan = A;
-    private static final int times = 2;
-    private static final int detectionId = 0;
+import java.util.Arrays;
 
-    private static TrajectorySequenceBuilder builder;
+public class MeepMeepTesting {
+    public static double TILE_WIDTH = 23.062;
 
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
             .setDimensions(14.75, 14)
-            .setConstraints(45, 45, Math.toRadians(180), Math.toRadians(180), 12.82)
-            .followTrajectorySequence(drive -> {
-                    builder = drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0));
-                    execute();
+            .setConstraints(45, 45, Math.toRadians(200), Math.toRadians(200), 12.82)
+            .followTrajectorySequence(
+                drive -> {
+                    Pose2d start = getTilePose("A1");
+                    Pose2d end = getJunctionPose("V5");
+                    Pose2d[] poses = getTransitionPoses(start, end, false);
+                    TrajectorySequenceBuilder builder = drive.trajectorySequenceBuilder(start);
+                    Arrays.stream(poses).forEach(builder::lineToLinearHeading);
                     return builder.build();
                 }
             );
@@ -39,62 +36,48 @@ public class MeepMeepTesting {
             .start();
     }
 
-    public static void execute() {
-        scoreStartCone();
-        executeChosenPlan();
-        park();
+    public static Pose2d getTilePose(String label){
+        return new Pose2d(
+            +(label.charAt(1) - (('3' + '4') / 2d)) * TILE_WIDTH,
+            -(label.charAt(0) - (('C' + 'D') / 2d)) * TILE_WIDTH
+        );
     }
 
-    public static void executeChosenPlan() {
-        if(plan == A) scoreStack(times);
-        if(plan == B) prepareToPark();
+    public static Pose2d getJunctionPose(String label){
+        return new Pose2d(
+            +(label.charAt(1) - '3') * TILE_WIDTH,
+            -(label.charAt(0) - 'X') * TILE_WIDTH
+        );
     }
 
-    public static void prepareToPark() {
-        turn(1, adapt(90));
+    public static Pose2d[] getTransitionPoses(Pose2d start, Pose2d end, boolean y1st){
+        Pose2d startTile = new Pose2d(
+            nearestTile(start.getX(), 0.5),
+            nearestTile(start.getY(), 0.5)
+        );
+
+        Pose2d endTile = new Pose2d(
+            nearestTile(end.getX() - startTile.getX(), 0) + startTile.getX(),
+            nearestTile(end.getY() - startTile.getY(), 0) + startTile.getY()
+        );
+
+        Pose2d midTile = new Pose2d(
+            y1st ? startTile.getX() : endTile.getX(),
+            y1st ? endTile.getY() : startTile.getY()
+        );
+
+        end = new Pose2d(
+            end.getX(),
+            end.getY(),
+            Math.atan2(end.getY() - midTile.getY(), end.getX() - midTile.getX())
+        );
+
+        return new Pose2d[] { midTile, end };
     }
 
-    public static void scoreStartCone() {
-        move(0, 1, 4);
-        move(adapt(1), 0, 26);
-        move(0, 1, 24);
-        turn(1, adapt(-45));
-        move(0, 1, 11.5);
-        move(0, -1, 11.5);
-    }
-
-    public static void scoreStack(int times) {
-        turn(1, 0);
-        move(0, 1, 24);
-        turn(1, adapt(90));
-
-        while (--times >= 0) {
-            move(0, 1, 49.5);
-            move(0, -1, 49.5);
-            turn(1, adapt(-135));
-            move(0, 1, 11.5);
-            move(0, -1, 11.5);
-            turn(1, adapt(90));
-        }
-    }
-
-    public static void park() {
-        boolean isLeft = side == LEFT;
-        if (detectionId == 0) move(0, 1, isLeft ? 46 : 0);
-        if (detectionId == 1) move(0, 1, 24);
-        if (detectionId == 2) move(0, 1, isLeft ? 0 : 46);
-    }
-
-    public static double adapt(double value) {
-        return side.sign * value;
-    }
-
-    public static void turn(double power, double heading) {
-        builder.turn(Math.toRadians(power * heading));
-    }
-
-    public static void move(double strafe, double forward, double distance) {
-        if (strafe != 0) builder.strafeRight(strafe * distance);
-        if (forward != 0) builder.forward(forward * distance);
+    private static double nearestTile(double value, double offset) {
+        double sign = value == 0 ? 1 : value / Math.abs(value);
+        double tiles = Math.floor(Math.abs(value) / TILE_WIDTH) + offset;
+        return sign * tiles * TILE_WIDTH;
     }
 }
