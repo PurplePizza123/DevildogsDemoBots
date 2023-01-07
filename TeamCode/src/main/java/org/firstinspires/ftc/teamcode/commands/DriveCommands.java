a98e4b5af54b47146a7afbd08a057cf5a43ba7fa
Devildogs11206 <devildogs11206@gmail.com>
2023-01-07 16:02:54 -0600

    fixed minor bug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DriveCommands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DriveCommands.java
@@ -14,6 +14,8 @@ import java.util.function.DoubleSupplier;
 
 @SuppressWarnings("unused")
 public class DriveCommands extends Commands {
+    private static final double INTAKE_OFFSET = -7;
+
     public Command setDrivePower(DriveSubsystem.DrivePower drivePower) {
         return new InstantCommand(() -> subsystems.drive.setDrivePower(drivePower), subsystems.drive);
     }
@@ -36,39 +38,39 @@ public class DriveCommands extends Commands {
         return complete(() -> subsystems.drive.turn(power, heading));
     }
 
-    public Command toPose(Pose2d pose) {
+    public Command toPose(Pose2d pose, double offset, boolean y1st) {
         return drive.complete(
                 () -> subsystems.drive.to(
                  subsystems.nav.getTransitionPoses(
                      subsystems.drive.getPose(),
-                     pose, 0, true
+                     pose, offset, y1st
                  )
             )
         );
     }
 
     public Command toTile(String label) {
-        return toPose(subsystems.nav.getTilePose(label));
+        return toPose(subsystems.nav.getTilePose(label), 0, true);
     }
 
     public Command toJunction(String label) {
-        return toPose(subsystems.nav.getJunctionPose(label)).alongWith(
+        return toPose(subsystems.nav.getJunctionPose(label), INTAKE_OFFSET, true).alongWith(
             lift.toJunction(Junction.get(label))
         );
     }
 
     public Command toStack(Alliance alliance, Side side) {
-        return toPose(subsystems.nav.getStackPose(alliance, side));
+        return toPose(subsystems.nav.getStackPose(alliance, side), INTAKE_OFFSET, true);
     }
 
     public Command toSubstation(Alliance alliance, Side side) {
-        return toPose(subsystems.nav.getSubstationPose(alliance, side)).alongWith(
+        return toPose(subsystems.nav.getSubstationPose(alliance, side), INTAKE_OFFSET, true).alongWith(
                 lift.toIntake(0)
         );
     }
 
     public Command toTerminal(Alliance alliance, Side side) {
-        return toPose(subsystems.nav.getTerminalPose(alliance, side)).alongWith(
+        return toPose(subsystems.nav.getTerminalPose(alliance, side), INTAKE_OFFSET, true).alongWith(
                 lift.toIntake(0)
         );
     }
