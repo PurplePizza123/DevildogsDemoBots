d75794da03b67b3554d4bf3de479457d3771eb6b
Devildogs11206 <devildogs11206@gmail.com>
2023-02-10 20:40:29 -0600

    reintroducing separate auto opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoOpMode.java
@@ -2,17 +2,16 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import static org.firstinspires.ftc.teamcode.game.Config.config;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-
 import org.firstinspires.ftc.teamcode.controllers.MenuController;
+import org.firstinspires.ftc.teamcode.game.Alliance;
+import org.firstinspires.ftc.teamcode.game.Side;
 
-@Autonomous(name = "Autonomous")
-@SuppressWarnings("unused")
 public class AutoOpMode extends OpMode {
-    @Override
-    public void initialize() {
+    public void initialize(Alliance alliance, Side side) {
         super.initialize();
 
+        config.alliance = alliance;
+        config.side = side;
         config.stacks = 1;
         config.delay = 0;
         config.navSamples = 50;
