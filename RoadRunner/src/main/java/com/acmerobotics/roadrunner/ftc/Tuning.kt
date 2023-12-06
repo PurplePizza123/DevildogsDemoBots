1feb835a537918e53b75e96db13e27b0add238fc
Devildogs11206 <devildogs11206@gmail.com>
2023-12-06 06:10:43 -0600

    road-runner, fixing omni configuration for LateralRampLogger

--- RoadRunner/src/main/java/com/acmerobotics/roadrunner/ftc/Tuning.kt
+++ RoadRunner/src/main/java/com/acmerobotics/roadrunner/ftc/Tuning.kt
@@ -413,9 +413,9 @@ class LateralRampLogger(val dvf: DriveViewFactory) : LinearOpMode() {
             setMotorPower(view.leftMotors[1], -1, data.backLeftPower)
             setMotorPower(view.rightMotors[1], +1, data.backRightPower)*/
             // Omni Configuration
-            setMotorPower(view.leftMotors[0], -1, data.frontLeftPower)
+            setMotorPower(view.leftMotors[0], +1, data.frontLeftPower)
             setMotorPower(view.rightMotors[0], +1, data.frontRightPower)
-            setMotorPower(view.leftMotors[1], -1, data.backLeftPower)
+            setMotorPower(view.leftMotors[1], +1, data.backLeftPower)
             setMotorPower(view.rightMotors[1], +1, data.backRightPower)
 
             data.voltages.values.add(view.voltageSensor.voltage)
