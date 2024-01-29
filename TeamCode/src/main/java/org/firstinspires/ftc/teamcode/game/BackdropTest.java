39a3e4607a66c3386554ed5b5a812dd50006ee1c
Devildogs11206 <devildogs11206@gmail.com>
2024-01-29 15:42:01 -0600

    started adding lift heights but need brandon to check it out

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/game/BackdropTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/game/BackdropTest.java
@@ -5,7 +5,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum BackdropTest {
-    GROUND(4.0), LOW(13.5), MEDIUM(23.5), HIGH(33.5);
+    L1(7.25), L2(L1 + PIXEL_DIAGONAL_HEIGHT), L3(L2 + PIXEL_DIAGONAL_HEIGHT), L4(L3 + PIXEL_DIAGONAL_HEIGHT),
+         L5(L4 + PIXEL_DIAGONAL_HEIGHT), L6(L5 + PIXEL_DIAGONAL_HEIGHT), L7(L6 + PIXEL_DIAGONAL_HEIGHT), L8(L7 + PIXEL_DIAGONAL_HEIGHT),
+         L9(L8 + PIXEL_DIAGONAL_HEIGHT), L10(L9 + PIXEL_DIAGONAL_HEIGHT), L11(L10 + PIXEL_DIAGONAL_HEIGHT); //TODO These are all to mid not top or above
+
+    public final double PIXEL_DIAGONAL_HEIGHT = 3.3499106299;
 
     public final double height;
 
@@ -15,12 +19,12 @@ public enum BackdropTest {
 
     private static final Map<String, BackdropTest> map = new HashMap<>();
 
-//    static {
-//        Arrays.asList("V1", "V3", "V5", "X1", "X3", "X5", "Z1", "Z3", "Z5").forEach(junction -> map.put(junction, GROUND));
-//        Arrays.asList("V2", "V4", "W1", "W5", "Y1", "Y5", "Z2", "Z4").forEach(junction -> map.put(junction, LOW));
-//        Arrays.asList("W2", "W4", "Y2", "Y4").forEach(junction -> map.put(junction, MEDIUM));
-//        Arrays.asList("W3", "X2", "X4", "Y3").forEach(junction -> map.put(junction, HIGH));
-//    }
+    static {
+        Arrays.asList("L1").forEach(backdrop -> map.put(backdrop, L1));
+        Arrays.asList("V2", "V4", "W1", "W5", "Y1", "Y5", "Z2", "Z4").forEach(junction -> map.put(junction, LOW));
+        Arrays.asList("W2", "W4", "Y2", "Y4").forEach(junction -> map.put(junction, MEDIUM));
+        Arrays.asList("W3", "X2", "X4", "Y3").forEach(junction -> map.put(junction, HIGH));
+    }
 
     public static BackdropTest get(String label) {
         return map.get(label);
